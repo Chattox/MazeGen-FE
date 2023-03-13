5fd82d271308b7d16615bff57b3410e425fd1265
Chattox <conradlineker@gmail.com>
2023-03-13 12:57:00 +0000

    feat(drawMaze): return maze as image

--- src/components/MazeDisplay.tsx
+++ src/components/MazeDisplay.tsx
@@ -1,22 +1,21 @@
 import { getAPI } from '../utils/api';
 import { useEffect, useState } from 'react';
-import { Loader, Grid, Container } from '@mantine/core';
+import { Loader, Container, Image } from '@mantine/core';
+
+import { drawMaze } from '../utils/drawMaze';
 
 export const MazeDisplay = () => {
-  const [maze, setMaze] = useState([[]]);
-  const [mazeHeight, setMazeHeight] = useState(0);
-  const [mazeWidth, setMazeWidth] = useState(0);
+  const [mazeImgUrl, setMazeImgUrl] = useState('');
   const [isLoaded, setIsLoaded] = useState(false);
 
   const getMaze = () =>
     getAPI('get-maze?height=35&width=35').then((res) => {
       if (res.status === 200) {
-        setMaze(res.data);
-        setMazeHeight(res.data.length);
-        setMazeWidth(res.data[0].length);
+        setMazeImgUrl(drawMaze(res.data));
         setIsLoaded(true);
       } else {
-        console.log('Status not 200');
+        console.log(res.status);
+        console.log(res.response);
       }
     });
 
@@ -25,21 +24,5 @@ export const MazeDisplay = () => {
     //eslint-disable-next-line
   }, []);
 
-  return (
-    <Container maw={`${mazeWidth}rem`} mah={`${mazeHeight}rem`}>
-      {isLoaded ? (
-        <Grid columns={mazeWidth} gutter={1} maw={`${mazeWidth}rem`} mah={`${mazeHeight}rem`}>
-          {maze.map((row) =>
-            row.map((tile) => (
-              <Grid.Col span={1} h="16px" w="16px">
-                {tile}
-              </Grid.Col>
-            ))
-          )}
-        </Grid>
-      ) : (
-        <Loader color="pink" />
-      )}
-    </Container>
-  );
+  return <Container>{isLoaded ? <Image src={mazeImgUrl} /> : <Loader color="pink" />}</Container>;
 };
