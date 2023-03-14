bb810531737572fac3b2ba352d2c491e8fb602b1
Chattox <conradlineker@gmail.com>
2023-03-14 13:14:27 +0000

    feat(mazecontrols): min/max values, styling

--- src/components/maze/MazeControls.tsx
+++ src/components/maze/MazeControls.tsx
@@ -1,10 +1,21 @@
-import { Button, Container, NumberInput } from '@mantine/core';
+import { Button, Container, createStyles, Group, Text, NumberInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { MazeProps } from './MazeContainer';
+
+const useStyles = createStyles((theme) => ({
+  'num-picker': {
+    maxWidth: '80px',
+  },
+  description: {
+    marginTop: theme.spacing.xs,
+    marginBottom: theme.spacing.md,
+  },
+}));
 
 export const MazeControls = (props: {
   setMazeGenProps: React.Dispatch<React.SetStateAction<MazeProps>>;
 }) => {
+  const { classes } = useStyles();
   const mazeForm = useForm({
     initialValues: {
       height: 15,
@@ -19,18 +30,32 @@ export const MazeControls = (props: {
           props.setMazeGenProps({ ...values });
         })}
       >
-        <NumberInput
-          defaultValue={15}
-          placeholder="Height"
-          label="Height"
-          {...mazeForm.getInputProps('height')}
-        />
-        <NumberInput
-          defaultValue={15}
-          placeholder="Width"
-          label="Width"
-          {...mazeForm.getInputProps('width')}
-        />
+        <Group position="center">
+          <NumberInput
+            defaultValue={15}
+            placeholder="Height"
+            label="Height"
+            min={3}
+            max={89}
+            step={2}
+            className={classes['num-picker']}
+            {...mazeForm.getInputProps('height')}
+          />
+
+          <NumberInput
+            defaultValue={15}
+            placeholder="Width"
+            label="Width"
+            min={3}
+            max={89}
+            step={2}
+            className={classes['num-picker']}
+            {...mazeForm.getInputProps('width')}
+          />
+        </Group>
+        <Text c="dimmed" fz="sm" className={classes.description}>
+          Min: 3, Max: 89. Values must be odd
+        </Text>
         <Button type="submit">Generate</Button>
       </form>
     </Container>
